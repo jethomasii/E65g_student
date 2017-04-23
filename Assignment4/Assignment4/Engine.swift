5a036e687e889786851dba9349ebe260aa09bffb
James Thomas <jethomasii@gmail.com>
2017-04-23 16:14:33 -0500

    Possibly implemented standardEngine class, still have to wire everything up

---
+++ Assignment4/Assignment4/Engine.swift
@@ -0,0 +1,54 @@
+//
+//  EngineProtocol.swift
+//  Assignment4
+//
+//  Created by James Thomas on 4/23/17.
+//  Copyright © 2017 Harvard Division of Continuing Education. All rights reserved.
+//
+
+import Foundation
+
+public protocol EngineDelegate {
+    func engineDidUpdate(withGrid: GridProtocol)
+}
+
+public protocol EngineProtocol {
+    var delegate: EngineDelegate? { get set }
+    var grid: GridProtocol { get }
+    var refreshRate: Double { get set }
+    var refreshTimer: Timer? { get set }
+    var rows: Int { get set }
+    var cols: Int { get set }
+    init(rows: Int, cols: Int)
+    func step() -> GridProtocol
+}
+
+extension EngineProtocol {
+    var refreshRate: Double { return 0.0 }
+}
+
+class StandardEngine: EngineProtocol {
+    var rows: Int
+    var cols: Int
+    var grid: GridProtocol
+    var refreshTimer: Timer?
+    var refreshRate: Double = 0.0
+    var delegate: EngineDelegate?
+
+    
+    required init(rows: Int, cols: Int) {
+        self.rows = rows
+        self.cols = cols
+        self.grid = Grid (rows, cols, cellInitializer: emptyInitializer)
+    }
+    
+    func step() -> GridProtocol {
+        return grid.next()
+    }
+    
+    
+}
+
+public func emptyInitializer(pos: GridPosition) -> CellState {
+    return .empty
+}
